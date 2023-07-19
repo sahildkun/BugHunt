b235d02a064aa803c4497b455fcd6feadafabd57
sahildkun <sahilsahoo23@gmail.com>
2023-07-19 10:51:00 +0530

    2nd

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import { options } from "../api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
-import UserCard from "../components/UserCard"
+
 import { redirect } from "next/navigation"
 
 export default async function ServerPage() {
@@ -12,7 +12,7 @@ export default async function ServerPage() {
 
     return (
         <section className="flex flex-col gap-6">
-            <UserCard user={session?.user} pagetype={"Server"} />
+            {/* <UserCard user={session?.user} pagetype={"Server"} /> */}
         </section>
     )
 
